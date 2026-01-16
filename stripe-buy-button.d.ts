e41d02e430fa4bf128b55e638dbf639435d8963f
structura-dev <hello@getstructura.com>
2026-01-16 16:44:37 -0500

    defect fixes

--- stripe-buy-button.d.ts
+++ stripe-buy-button.d.ts
@@ -1,13 +1,17 @@
 import type React from "react";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "stripe-buy-button": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      "buy-button-id": string;
-      "publishable-key": string;
-    };
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "stripe-buy-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        "buy-button-id": string;
+        "publishable-key": string;
+      };
+    }
   }
 }
+
+export {};
