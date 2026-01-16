091ae3510ffe604968acf95df2b174648badef8b
structura-dev <hello@getstructura.com>
2026-01-16 17:01:41 -0500

    yt ids updated

--- app/constants.ts
+++ app/constants.ts
@@ -5,6 +5,6 @@ export const STRIPE_MONTHLY_BUTTON_ID = "buy_btn_1SqKIDBGQwUWprZQzr45MDKx";
 export const STRIPE_PROOF_PACK_URL = "PASTE_YOUR_STRIPE_PROOF_PACK_URL";
 export const SUPPORT_EMAIL = "hello@getstructura.com";
 
-export const SAMPLE_VIDEO_1 = "9JmQmVgfugQ";
-export const SAMPLE_VIDEO_2 = "hZVqLhvPWjM";
-export const SAMPLE_VIDEO_3 = "xA9vfNqU1Go";
+export const SAMPLE_VIDEO_1 = "Q2Se5YP2OTo";
+export const SAMPLE_VIDEO_2 = "QXMTDCtYUC0";
+export const SAMPLE_VIDEO_3 = "lVBXWpC3Wtc";
